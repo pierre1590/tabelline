1ec2f7c2026d3ebc30d14a2e50ca37c0a5f24df8
Piero Sabino <piero.sa@icloud.com>
2021-10-28 23:39:58 +0200

    Update

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ function updateDisplay(){
 
 function calcolaTabellina(){
     var numTabellina = document.getElementById('number').value;
+
     for(var i=0; i<11; i++){
-        stringa += (i*numTabellina) + ' ';
+        stringa += (i*numTabellina) + '  ';
         updateDisplay();
         displayElem.style.background = "#f01";
         displayElem.style.borderRadius = "30px";
-        displayElem.style.padding = "10px";
+        displayElem.style.padding = "15px";
         displayElem.style.textAlign = "center";
-        displayElem.style.borderRadius = "30px";
-        displayElem.style.fontSize = "30px";
+        displayElem.style.fontSize = "24px";
     }
 }
 
